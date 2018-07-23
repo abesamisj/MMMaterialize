a9c2b8fdc9f2ba282e71d9d3ae2502855694f9c6
Jhiggy Abesamis <jhiggy.abesamis@outlook.com>
2018-07-23 16:55:02 +1200

    materialise

--- MMM3/Controllers/HomeController.cs
+++ MMM3/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,23 +10,59 @@ namespace MMM3.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Pricing()
+        {
+            return View();
+        }
+
+        public ActionResult Foundation()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
         {
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
+        public ActionResult HowItWorks()
+        {
+            return View();
+        }
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(string name, string email, string message)
+        {
+            SmtpClient client = new SmtpClient("mail.1solvesolutions.co.nz");
+            //If you need to authenticate
+            client.Credentials = new NetworkCredential("abesamisj@gmail.com", "Passw0rd1");
+
+            var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(email);
+            mailMessage.To.Add(new MailAddress("enquiries@1solvesolutions.co.nz"));
+            mailMessage.Subject = "Interest from website";
+            mailMessage.Body = string.Format(body, name, email, message);
+            mailMessage.IsBodyHtml = true;
+
+            client.Send(mailMessage);
+
+            TempData["Message"] = "Message sent. Thank you for your email. We will respond to you shortly.";
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
